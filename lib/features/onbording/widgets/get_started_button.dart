848f625684ec835e39c7627ab04f7b546277fda8
Mohamed Yasser <my1844401@gmail.com>
2024-11-06 06:01:02 +0200

    ss

---
+++ lib/features/onbording/widgets/get_started_button.dart
@@ -0,0 +1,34 @@
+import 'package:docdoc_app/core/helper/extentions.dart';
+import 'package:docdoc_app/core/routers/routes.dart';
+import 'package:docdoc_app/core/theming/colos.dart';
+import 'package:docdoc_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: WidgetStateProperty.all(
+          const Size(double.infinity, 52),
+        ),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+      child: Text(
+        'Get Started',
+        style: TextStyles.font16WhiteMedium,
+      ),
+    );
+  }
+}
